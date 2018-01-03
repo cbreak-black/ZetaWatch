4574476b0681944255d895636672dff137b910dc
Gerhard Roethlin <git@the-color-black.net>
2018-01-03 15:23:18 +0100

    Helper uninstall script
    For debugging / development being able to uninstall the helper so it
    is reinstalled / updated without changing the version number is very
    useful. For users, being able to uninstall it might also be useful.

---
+++ uninstall-helper.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+launchctl unload /Library/LaunchDaemons/net.the-color-black.ZetaAuthorizationHelper.plist
+rm /Library/LaunchDaemons/net.the-color-black.ZetaAuthorizationHelper.plist
+rm /Library/PrivilegedHelperTools/net.the-color-black.ZetaAuthorizationHelper
